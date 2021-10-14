0030020a70925fe2077b01aa6bcefdc460fa8d9c
Alexandra Țihonschi <70908434+alexandratihonschi@users.noreply.github.com>
2021-10-14 18:18:30 +0300

    Add files via upload

---
+++ problema2.py
@@ -0,0 +1,13 @@
+w=str(input('introduceti mesajui '))
+for i in range(0,len(w)):
+    if ord(w[i])!=len(w)-1:
+        z=w.replace(chr(ord(w[i])), chr(ord(w[i]+1))
+    print('inlocuirea literei din sir prin urmatoarea din alfabet si variabila posibila', z)
+for i in range(0,len(w)):
+    if ord(w[i]!=len(w)-1):
+        c=w.replace('Z','A')
+for i in range(0,len(w)):
+    if ord(w[i]!=len(w)-1):
+        d=w.replace(' ','-')
+print('sirul de caractere in care z este inlocuit prin a', c)
+print('inlocuirea spatiilor prin -', d)
